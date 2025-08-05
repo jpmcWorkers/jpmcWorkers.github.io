6c243dae3514df2db03864d264a6b83e59e600a1
Ben Koop <nivoset@gmail.com>
2025-08-05 11:14:40 -0400

    try out

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nivoset.github.io',
+  base: '/workers.github.io',
   output: 'static',
 
   vite: {
